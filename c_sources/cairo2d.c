2ad11186366ce79e319bb7e67d73492b7281351a
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-15 06:15:18 +0200

    [cairo] draw_rectangle_rounded() from : cairographics.org/samples/rounded_rectangle/

--- c_sources/cairo2d.c
+++ c_sources/cairo2d.c
@@ -5,12 +5,42 @@
 
 // gcc -o cairo2d c_sources/*.c* $(pkgconf --cflags --libs gtk+-3.0) -mwindows && ./cairo2d # -mwindows only in Windows
 
-static gboolean draw_rectangle(GtkWidget *widget, cairo_t *cr, gpointer data)
+static gboolean draw_rectangle_basic(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
     // Set rectangle properties (e.g., position, size, color)
     cairo_set_source_rgb(cr, 0.5, 0.7, 0.9); // RGB color
     cairo_rectangle(cr, 50, 50, 100, 80);    // Rectangle coordinates and dimensions
     cairo_fill(cr);                          // Fill the rectangle
+
+    return FALSE; // Return FALSE to allow other handlers to draw
+}
+
+// https://cairographics.org/samples/rounded_rectangle/
+static gboolean draw_rectangle_rounded(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    /* a custom shape that could be wrapped in a function */
+    double x = 50, /* parameters like cairo_rectangle */
+        y = 50,
+           width = 100,
+           height = 80,
+           aspect = 1.0,               /* aspect ratio */
+        corner_radius = height / 10.0; /* and corner curvature radius */
+
+    double radius = corner_radius / aspect;
+    double degrees = G_PI / 180.0;
+
+    cairo_new_sub_path(cr);
+    cairo_arc(cr, x + width - radius, y + radius, radius, -90 * degrees, 0 * degrees);
+    cairo_arc(cr, x + width - radius, y + height - radius, radius, 0 * degrees, 90 * degrees);
+    cairo_arc(cr, x + radius, y + height - radius, radius, 90 * degrees, 180 * degrees);
+    cairo_arc(cr, x + radius, y + radius, radius, 180 * degrees, 270 * degrees);
+    cairo_close_path(cr);
+
+    cairo_set_source_rgb(cr, 0.5, 0.5, 1);
+    cairo_fill_preserve(cr);
+    cairo_set_source_rgba(cr, 0.5, 0, 0, 0.5);
+    cairo_set_line_width(cr, 10.0);
+    cairo_stroke(cr);
 
     return FALSE; // Return FALSE to allow other handlers to draw
 }
@@ -22,7 +52,7 @@ int main(int argc, char *argv[])
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     GtkWidget *drawing_area = gtk_drawing_area_new();
 
-    g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_rectangle), NULL);
+    g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_rectangle_rounded), NULL);
 
     gtk_container_add(GTK_CONTAINER(window), drawing_area);
 
